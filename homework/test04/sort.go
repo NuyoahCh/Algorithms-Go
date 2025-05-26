00087d92f4309dfda74eb005dd5a53f4f283582a
NuyoahCh <springoflotus@gmail.com>
2025-05-26 08:51:09 +0800

    实现归并排序

--- homework/test04/sort.go
+++ homework/test04/sort.go
@@ -43,3 +43,53 @@ func swap(array []int, i, j int) {
 	array[i] = array[j]
 	array[j] = temp
 }
+
+// 归并排序
+func mergeSort(array []int) []int {
+	if array == nil || len(array) == 0 {
+		return array
+	}
+	helper := make([]int, len(array))
+	mergeSortFunc(array, helper, 0, len(array)-1)
+	return array
+}
+
+func mergeSortFunc(array []int, helper []int, left int, right int) {
+	if left >= right {
+		return
+	}
+	mid := left + (right-left)/2
+	mergeSortFunc(array, helper, left, mid)
+	mergeSortFunc(array, helper, mid+1, right)
+	merge(array, helper, left, mid, right)
+}
+
+func merge(array []int, helper []int, left int, mid int, right int) {
+	for i := left; i <= right; i++ {
+		helper[i] = array[i]
+	}
+	i := left
+	j := mid + 1
+	index := left
+
+	for i <= mid && j <= right {
+		if helper[i] <= helper[j] {
+			array[index] = helper[i]
+			i++
+		} else {
+			array[index] = helper[j]
+			j++
+		}
+		index++
+	}
+	for i <= mid {
+		array[index] = helper[i]
+		i++
+		index++
+	}
+	for j <= right {
+		array[index] = helper[j]
+		j++
+		index++
+	}
+}
